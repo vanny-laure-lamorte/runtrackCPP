b06ac46c4f028496dbb171ecaa5d24fb32f07fe8
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-29 14:35:41 +0200

    [Terminé]jour01, job04

---
+++ jour01/job04/main.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std; 
+
+int main(void)
+{
+    int a, b;
+
+    cout << "Entrez le premier nombre : ";
+    cin >> a;
+    cout << "Entrez le second nombre : ";
+    cin >> b;
+
+    int somme = a+b;
+
+    cout << "La somme de " << a << " et de "<< b << " est : " << somme << endl;
+
+}
