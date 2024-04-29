31f5a6146f7b0df1e08ab3ae527d09c8a702f2e4
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-29 13:42:02 +0200

    [Terminé]jour01, job0

---
+++ jour01/job0/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+    int a, b, c, d, x, y, i, n, p, d;
+
+    a = x + 5;
+    a = x = y + 2;
+    a = x == y; 
+    a<b && c<d; 
+    (i++) * (n+p); 
+
+    return 0;
+}
