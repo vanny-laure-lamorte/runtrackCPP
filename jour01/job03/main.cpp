e80d2434540c76ad5db87f939f0e7e3207a8ae7c
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-29 14:17:36 +0200

    [Terminé]jour01, job03

---
+++ jour01/job03/main.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std; 
+
+int main(void)
+{
+    int N;
+    cout <<"Entrez une valeur: "; 
+    cin >> N ; 
+
+    while (N !=0){
+        cout << "Hello World !" << endl;
+        N--;
+    }
+    
+    return 0;
+}
