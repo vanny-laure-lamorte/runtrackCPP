f647f0bf17511366afaac73c3238f3b4aac5ca5f
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-30 13:46:18 +0200

    [Terminé]jour02, job05

---
+++ jour02/job05/main.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+
+using namespace std; 
+#include <cctype>
+
+int main (void){
+
+    int a;
+
+    do {  
+  
+    cout << "Saisir une note"  << endl;
+    cin >> a;
+
+        if (a <= 20 ) {
+
+            if (a > 10 ) {
+                cout << "Valid"<<char(130)<< endl;
+            }
+
+            else {
+                cout <<"Non valid" <<char(130)<< endl;
+                }
+
+        }
+        else {
+            cout <<"Veuillez saisir une note entre 0 et 20"<< endl;
+
+            
+        }
+
+        } while (a > 20);
+
+}
+
