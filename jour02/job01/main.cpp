bee96a1c1b900d4f789a729c681f428f14262ab7
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-30 10:19:58 +0200

    [Terminé]jour02, job01

---
+++ jour02/job01/main.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std; 
+
+int main(void)
+{
+    int n;
+
+    do
+    {     
+    cout <<"Donnez un nombre > 0 : "; 
+    cin >> n; 
+    } while (n <= 0);
+
+    return 0;
+
+}
+    
+
+   
