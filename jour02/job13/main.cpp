d6b62ff76a0792a2fbc5428f7c00fe8448f29442
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-02 13:42:54 +0200

    [Terminé]jour02, job13

--- jour02/job13/main.cpp
+++ jour02/job13/main.cpp
@@ -1,13 +1,69 @@
 # include <iostream>
+#include <iomanip>
 
 using namespace std; 
-#include <cmath>
 
 int main (void){
 
+    int i = 1;
+    int j;
+    int valeur;
+
+    // 1er ligne
+
+    printf("\t\t|");
+
+    for (int a = 1 ; a <= 10; a++){
+        printf("\t%d", a);
+    }
+
+    printf("\n");
+
+    // 2 ème ligne avec des tirets 
+
+    for (int a = 1 ; a <= 20; a++){
+        printf("-----", a);
+    }
 
 
-  }
+    // Table de multiplication
+
+    while (i <= 10) {
+
+        printf("\n");
+
+        printf("\t%d", i);
+        
+        printf("\t|");
+
+
+        j = 1;
+
+        while (j <= 10){
+            valeur = i*j;
+            printf("\t%d", valeur);
+            j++;
+        }
+
+        i++;
+    }
+
+
+}
+
+
+
+
+//     for ( a = 0; a <= 9; a++) {
+//         cout << a  << endl;
+//     };
+
+//     for ( a = 0; a <= 9; a++ ) {       
+//         cout << b[a] << "-"  << endl;
+//     };
+
+
+//   }
  
        
 
