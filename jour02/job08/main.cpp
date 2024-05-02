3b8e5fffb926e61d542b15b61f923ee6cf38ac3d
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-02 09:41:14 +0200

    [Terminé]jour02, job08

---
+++ jour02/job08/main.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std; 
+#include <cctype>
+
+int main (void){
+
+    int a, b;
+  
+    cout << "Saisir un entier a = ";
+    cin >> a;
+    cout << "Saisir un entier b = ";
+    cin >> b;
+
+    while (a <= b) {
+    cout <<a<< endl;
+    a++;
+    
+    }
+
+}
+       
+
+
+
