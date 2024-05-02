669eb1710839c5076541b6bb4f8d3381a072adfa
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-02 11:21:02 +0200

    [Terminé]jour02, job11

---
+++ jour02/job11/main.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+
+using namespace std; 
+#include <cmath>
+
+int main (void){
+
+    float a;
+    float i = 0;
+    float f = 1;
+
+    cout << "Saisir une valeur a = ";
+    cin >> a;
+
+    for (i = 1; i <= a; i++) {
+
+
+        f = f * i;
+
+    };  
+   
+    cout << "Le factoriel de " << a << " est : " << f << endl;
+
+  }
+ 
+       
+
+  
+    
+
+
+
