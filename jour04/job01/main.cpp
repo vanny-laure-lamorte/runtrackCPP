8d45344a7243123651e816db823e2da35bf7b1a0
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-05 15:09:55 +0200

    [Terminé] jour04, job01

---
+++ jour04/job01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+int number = 2019; 
+int *ptr = &number; 
+
+cout << "La valeur de number est : " << *ptr << endl;
+
+return 0;
+
+}
+
