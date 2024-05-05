cb25d8e514543d27f490e4d1e749e9d721f89375
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-05 12:29:18 +0200

    [Terminé]jour03, job04

---
+++ jour03/job04/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    string chaine = "Vive la plateforme !";
+
+    int longeur = chaine.length();
+
+    cout << chaine << endl;
+    cout << "La taille est de : " << longeur << endl;
+   
+   return 0;
+}
