4d5ac4da401b31c044c1aec9cd06de057eb9cfa4
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-05-05 19:31:01 +0200

    [Terminé] jour04, job14

---
+++ jour04/job14/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+    int x = 12; 
+    cout << "La valeur de x AVANT modification : " << x << endl;
+
+    int &ref_x = x;
+    ref_x = 21; 
+
+    cout << "La valeur de x APRES modification : " << x << endl; 
+
+    return 0;
+}
